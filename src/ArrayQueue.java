682d4f6d0c0432ebcd682a7e9683b889fad50bd6
idancoh30 <102730179+idancoh30@users.noreply.github.com>
2022-06-12 18:12:16 +0300

    verision A with documentaiton.

--- src/ArrayQueue.java
+++ src/ArrayQueue.java
@@ -1,6 +1,10 @@
 import java.lang.reflect.Method;
 import java.util.Iterator;
 
+/**
+ * Queue to order elements in a FIFO (=First in first out) manner, implemented by cyclic array.
+ * @param <E> Generic type for queue.
+ */
 public class ArrayQueue<E extends Cloneable> implements Queue<E> {
     private int front;
     private int rear;
@@ -8,30 +12,51 @@ public class ArrayQueue<E extends Cloneable> implements Queue<E> {
     private int capacity;
     private Cloneable[] data;
 
-
+    /**
+     * Initialize new array queue.
+     * @param capacity - maximum queue capacity.
+     * @throws NegativeCapacityException if queue's given size is negative.
+     */
     public ArrayQueue(int capacity) {
         if (capacity < 0)
             throw new NegativeCapacityException();
         this.capacity = capacity;
         this.data = new Cloneable[capacity];
-        this.rear = -1;
-        this.front = -1;
-        this.numElements = 0;
+        this.rear = -1; // track the last element of the queue, when queue is empty, rear is equal to -1.
+        this.front = -1; // track the first element of the queue, when queue is empty, front is equal to -1.
+        this.numElements = 0; // number of non-null elements in queue.
     }
 
+    /**
+     * Check whether queue is full.
+     * @return true if queue is full, false otherwise.
+     */
     public boolean isFull() {
         return (numElements == capacity);
     }
 
+    /**
+     * Check whether queue is empty.
+     * @return true if queue is empty, false otherwise.
+     */
     @Override
     public boolean isEmpty() {
         return (numElements == 0);
     }
 
+    /**
+     * Returns number of non-null elements in queue.
+     * @return number of non-null elements in queue.
+     */
     public int size() {
         return numElements;
     }
 
+    /**
+     * Retrieves, but does not remove the head of the queue.
+     * @return the head of the queue.
+     * @throws EmptyQueueException if queue is empty.
+     */
     @Override
     public E peek() {
         if (isEmpty())
@@ -39,12 +64,17 @@ public class ArrayQueue<E extends Cloneable> implements Queue<E> {
         return (E) data[front];
     }
 
+    /**
+     * Retrieves, and removes the head of the queue.
+     * @return the head of the queue.
+     * @throws EmptyQueueException if queue is empty.
+     */
     public E dequeue() {
         if (isEmpty()) {
             throw new EmptyQueueException();
         }
         Cloneable dequeued = data[front];
-        if (front == rear) {
+        if (front == rear) { // Only one element in queue, when removing it queue will be restored to it's initial status.
             data[front] = null;
             front = -1;
             rear = -1;
@@ -55,6 +85,11 @@ public class ArrayQueue<E extends Cloneable> implements Queue<E> {
         return (E) dequeued;
     }
 
+    /**
+     * Inserts the specified element into the queue.
+     * @param element - the element to be inserted into the queue.
+     * @throws QueueOverflowException if queue is full.
+     */
     @Override
     public void enqueue(E element) {
         if (isFull())
@@ -69,35 +104,52 @@ public class ArrayQueue<E extends Cloneable> implements Queue<E> {
         }
     }
 
+    /**
+     * Returns the capacity of the queue.
+     * @return queue's capacity.
+     */
     public int getCapacity() {
         return capacity;
     }
 
+    /**
+     * Returns element at the specified index.
+     * @param index - the index of the wanted element.
+     * @return - the element in the given array's index.
+     */
     public Cloneable getElement(int index) {
         return data[index];
     }
 
+    /**
+     * Creates and returns a deep copy of the queue.
+     * @return deep copy of the queue.
+     */
     @Override
     public ArrayQueue<E> clone() {
         try {
-            ArrayQueue<E> copy = (ArrayQueue<E>)super.clone();
+            ArrayQueue<E> copy = (ArrayQueue<E>)super.clone(); // Create shallow copy of array queue.
             Cloneable[] copiedData = new Cloneable[this.capacity];
             for(int i = 0; i < this.capacity; i++){
                 if (data[i] != null) {
-                    Class metaClass = data[i].getClass();
-                    Method m = metaClass.getMethod("clone");
+                    Class metaClass = data[i].getClass(); // get object's runtime class.
+                    Method m = metaClass.getMethod("clone"); // get clone method of current object.
                     copiedData[i] = (Cloneable)m.invoke(data[i]);
                 }
             }
             copy.data = copiedData;
             return copy;
         }
-        catch (Exception e)
+        catch (Exception e) // All possible exception are checked, thus inherits from Exception class.
         {
             return null;
         }
     }
 
+    /**
+     * Returns an iterator over the elements in the queue.
+     * @return an iterator over the elements in the queue.
+     */
     @Override
     public Iterator iterator() {
         return new ArrayQueueIterator(this);
--- src/ArrayQueueIterator.java
+++ src/ArrayQueueIterator.java
@@ -1,18 +1,35 @@
 import java.util.Iterator;
 
+/**
+ * ArrayQueueIterator allows iterating over an ArrayQueue.
+ * @param <E> generic type for ArrayQueueIterator.
+ */
 public class ArrayQueueIterator<E extends Cloneable> implements Iterator<E> {
     private ArrayQueue<E> arrayQueue;
     private int index;
 
+    /**
+     * Initialize a new array queue iterator.
+     * @param arrayQueue - the array queue to iterate.
+     */
     public ArrayQueueIterator(ArrayQueue<E> arrayQueue){
             this.arrayQueue = arrayQueue;
             this.index = 0;
     }
 
+    /**
+     * Returns true if the iteration has more elements, false otherwise.
+     * @return true if the iteration has more elements, false otherwise.
+     */
     @Override
     public boolean hasNext() {
         return (this.index < this.arrayQueue.getCapacity() && arrayQueue.getElement(index) != null);
     }
+
+    /**
+     * Returns the next element in the iteration.
+     * @return the next element in the iteration.
+     */
     @Override
     public E next() {
         E element = (E)this.arrayQueue.getElement(index);
